5dc345ac06a11d010c7dca7a3c8212ee019a8bea
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-03-24 23:05:28 -0400

    Initial commit

---
+++ app/src/main/java/com/gmail/orlandroyd/jetreddit/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.gmail.orlandroyd.jetreddit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gmail.orlandroyd.jetreddit.ui.theme.JetRedditTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetRedditTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetRedditTheme {
+        Greeting("Android")
+    }
+}
