74d61cfbd84eaf40936ce2bcc6641eb82fd6a519
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-03-27 11:58:51 -0400

    AppDrawerFooter

--- app/src/main/java/com/gmail/orlandroyd/jetreddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/gmail/orlandroyd/jetreddit/appdrawer/AppDrawer.kt
@@ -2,6 +2,7 @@ package com.gmail.orlandroyd.jetreddit.appdrawer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,6 +15,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -257,7 +259,54 @@ private fun ScreenNavigationButton(
  */
 @Composable
 private fun AppDrawerFooter(modifier: Modifier = Modifier) {
-    //TODO add your code here
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                start = 16.dp,
+                bottom = 16.dp,
+                end = 16.dp
+            )
+    ) {
+
+        val colors = MaterialTheme.colors
+        val (settingsImage, settingsText, darkModeButton) = createRefs()
+
+        Icon(
+            modifier = modifier.constrainAs(settingsImage) {
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+            },
+            imageVector = Icons.Default.Settings,
+            contentDescription = stringResource(id = R.string.settings),
+            tint = colors.primaryVariant
+        )
+
+        Text(
+            fontSize = 10.sp,
+            text = stringResource(R.string.settings),
+            style = MaterialTheme.typography.body2,
+            color = colors.primaryVariant,
+            modifier = modifier
+                .padding(start = 16.dp)
+                .constrainAs(settingsText) {
+                    start.linkTo(settingsImage.end)
+                    centerVerticallyTo(settingsImage)
+                }
+        )
+
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_moon),
+            contentDescription = stringResource(id = R.string.change_theme),
+            modifier = modifier
+                .clickable(onClick = { changeTheme() })
+                .constrainAs(darkModeButton) {
+                    end.linkTo(parent.end)
+                    bottom.linkTo(settingsImage.bottom)
+                },
+            tint = colors.primaryVariant
+        )
+    }
 }
 
 private fun changeTheme() {
