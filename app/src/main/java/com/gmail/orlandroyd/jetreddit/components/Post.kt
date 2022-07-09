1213bdd2f45ccfc577d890ebc23a1d92d0102d1c
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-07-09 13:10:36 -0400

    HomeScreen.kt modified to show Toast

--- app/src/main/java/com/gmail/orlandroyd/jetreddit/components/Post.kt
+++ app/src/main/java/com/gmail/orlandroyd/jetreddit/components/Post.kt
@@ -28,21 +28,31 @@ import com.gmail.orlandroyd.jetreddit.domain.model.PostModel
 import com.gmail.orlandroyd.jetreddit.domain.model.PostModel.Companion.DEFAULT_POST
 
 @Composable
-fun TextPost(post: PostModel) {
-    Post(post) {
+fun TextPost(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {},
+) {
+    Post(post, onJoinButtonClick) {
         TextContent(post.text)
     }
 }
 
 @Composable
-fun ImagePost(post: PostModel) {
-    Post(post) {
-        ImageContent(post.image ?: R.drawable.compose_course)
+fun ImagePost(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {},
+) {
+    Post(post, onJoinButtonClick) {
+        ImageContent(post.image!!)
     }
 }
 
 @Composable
-fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
+fun Post(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {},
+    content: @Composable () -> Unit = {},
+) {
     Card(shape = MaterialTheme.shapes.large) {
         Column(
             modifier = Modifier.padding(
@@ -50,7 +60,7 @@ fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
                 bottom = 8.dp
             )
         ) {
-            Header(post)
+            Header(post, onJoinButtonClick)
             Spacer(modifier = Modifier.height(4.dp))
             content.invoke()
             Spacer(modifier = Modifier.height(8.dp))
--- app/src/main/java/com/gmail/orlandroyd/jetreddit/screens/HomeScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/jetreddit/screens/HomeScreen.kt
@@ -1,36 +1,55 @@
 package com.gmail.orlandroyd.jetreddit.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.gmail.orlandroyd.jetreddit.components.ImagePost
+import com.gmail.orlandroyd.jetreddit.components.JoinedToast
 import com.gmail.orlandroyd.jetreddit.components.TextPost
 import com.gmail.orlandroyd.jetreddit.domain.model.PostModel
 import com.gmail.orlandroyd.jetreddit.domain.model.PostType
 import com.gmail.orlandroyd.jetreddit.viewmodel.MainViewModel
+import java.util.*
+import kotlin.concurrent.schedule
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
-    val posts: List<PostModel> by viewModel.allPosts.observeAsState(listOf())
-
-    LazyColumn(
-        modifier = Modifier.background(color = MaterialTheme.colors.secondary)
-    ) {
-        items(posts) {
-            if (it.type == PostType.TEXT) {
-                TextPost(it)
-            } else {
-                ImagePost(it)
+    val posts: List<PostModel>
+            by viewModel.allPosts.observeAsState(listOf())
+    var isToastVisible by remember { mutableStateOf(false) }
+    val onJoinClickAction: (Boolean) -> Unit = { joined ->
+        isToastVisible = joined
+        if (isToastVisible) {
+            Timer().schedule(3000) {
+                isToastVisible = false
             }
-            Spacer(modifier = Modifier.height(6.dp))
+        }
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.background(color =
+        MaterialTheme.colors.secondary)) {
+            items(posts) {
+                if (it.type == PostType.TEXT) {
+                    TextPost(it, onJoinButtonClick = onJoinClickAction)
+                } else {
+                    ImagePost(it, onJoinButtonClick = onJoinClickAction)
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+            }
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            JoinedToast(visible = isToastVisible)
         }
     }
 }
